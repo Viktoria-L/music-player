ba1c019ce6e146e1b5fe52ec1fbd3598a2173dc0
Viktoria L <113613194+Viktoria-L@users.noreply.github.com>
2024-06-06 11:35:09 +0200

    config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/music-player",
   plugins: [react()],
   build: {
     outDir: "dist",
   },
-  base: "./",
 });
