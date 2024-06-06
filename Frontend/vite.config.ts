cc86f74832a8e996f436af63961d2c24f035ab91
Viktoria L <113613194+Viktoria-L@users.noreply.github.com>
2024-06-06 16:34:27 +0200

    ....

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || "/",
+  base: process.env.NODE_ENV === "production" ? "/music-player/" : "/",
   plugins: [react()],
   build: {
     outDir: "dist",
